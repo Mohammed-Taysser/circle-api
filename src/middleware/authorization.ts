f0a30a3c816e8191ac6bf4033ce7e935d05d8939
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-08 00:33:20 +0300

    add authorization middleware

---
+++ src/middleware/authorization.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response } from 'express';
+import statusCode from 'http-status-codes';
+import { UserInRequest } from '../core/app';
+import { verifyToken } from '../utils/jwt';
+
+const authorization = async (
+  request: UserInRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  const token = request.headers['authorization'];
+
+  if (token?.startsWith('Bearer')) {
+    try {
+      const user = verifyToken(token.split(' ')[1]);
+
+      request.user = user;
+      next();
+    } catch (err) {
+      response
+        .status(statusCode.FORBIDDEN)
+        .json({ error: `you aren't authorize, token is invalid` });
+    }
+  } else {
+    response
+      .status(statusCode.UNAUTHORIZED)
+      .json({ error: 'no token provide' });
+  }
+};
+
+export default authorization;
