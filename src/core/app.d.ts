3f16231b397c720c6a7d39dcb4e9c9816f7e19aa
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-07 20:53:14 +0300

    add app types

---
+++ src/core/app.d.ts
@@ -0,0 +1,23 @@
+import { Document } from 'mongoose';
+import userSchema from '../schema/user.schema';
+import { Request } from 'express';
+
+interface User extends Document {
+  username: string;
+  role: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  email: string;
+  password: string;
+}
+
+declare global {
+  namespace Express {
+    interface UserInRequest {
+      user?: Record<string, any>;
+    }
+  }
+}
+
+export { User, UserInRequest };
