a644c7e2b5c49754a05f9dc2b565cb9b0effcd03
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-08 17:57:43 +0300

    add swagger startup

--- src/routes/helper.route.ts
+++ src/routes/helper.route.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import controller from '../controllers/helper.controller';
+import swaggerDocument from '../core/swagger.json';
 
 const router = express.Router();
 
-import controller from '../controllers/helper.controller';
-
 router.get('/health-check', controller.healthCheck);
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(swaggerDocument));
 
 export default router;
