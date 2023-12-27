2f25f0ed4dc0e588b226b14d2d02c0712602e273
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-27 22:39:34 +0200

    phase1 - create user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+import { hashPassword } from '../utils/password';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'username not provided!'],
+      unique: [true, 'username already exists!'],
+    },
+    role: {
+      type: String,
+      default: 'user',
+      enum: ['admin', 'user'],
+      required: [true, 'Please specify user role'],
+    },
+    firstName: { type: String, required: [true, 'first name not provided!'] },
+    lastName: { type: String, required: [true, 'last name not provided!'] },
+    avatar: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/mohammed-taysser/image/upload/v1654679434/paperCuts/authors/avatar-2_grpukn.png',
+    },
+    cover: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/mohammed-taysser/image/upload/v1657350049/lama/users/5437842_py0e8h.jpg',
+    },
+    email: {
+      type: String,
+      required: [true, 'email not provided!'],
+      unique: [true, 'email already exists!'],
+    },
+    password: { type: String, required: [true, 'password not provided!'] , select:false},
+    badges: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Badge',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await hashPassword(this.password);
+  }
+});
+
+export default mongoose.model('User', userSchema);
