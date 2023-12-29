889ad845a02bf5be17e0d5c33fa7163e6d540dfa
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-29 17:42:51 +0200

    phase1 - create authorization middleware

---
+++ src/middleware/authorization.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import statusCode from 'http-status-codes';
+import service from '../services/user.service';
+import { verifyToken } from '../utils/jwt';
+import { IRequest } from 'types/user';
+
+const authorization = async (
+  req: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const request = req as IRequest
+  const token = request.headers['authorization'];
+
+  if (token?.startsWith('Bearer')) {
+    try {
+      const user = verifyToken(token.split(' ')[1]);
+
+      const userDB = await service.getById(String(user._id));
+
+      if (userDB) {
+        request.user = userDB
+      } else {
+        response
+          .status(statusCode.FORBIDDEN)
+          .json({ error: `you aren't authorize, token is invalid` });
+      }
+
+      next();
+    } catch (err) {
+      response
+        .status(statusCode.FORBIDDEN)
+        .json({ error: `you aren't authorize, token is invalid` });
+    }
+  } else {
+    response
+      .status(statusCode.UNAUTHORIZED)
+      .json({ error: 'no token provide' });
+  }
+};
+
+export default authorization;
