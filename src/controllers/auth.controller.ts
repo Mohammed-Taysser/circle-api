bc6a77f989bf59eb00fb6d9f30fafc770c277ae3
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-29 17:42:03 +0200

    phase1 - create auth controller

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import statusCode from 'http-status-codes';
+import { IRequest } from 'types/user';
+import service from '../services/user.service';
+import { generateToken } from '../utils/jwt';
+
+async function register(request: Request, response: Response) {
+  try {
+    const user = await service.save(request.body);
+
+    const token = await generateToken(user);
+
+    response.status(statusCode.CREATED).json({ user, token });
+  } catch (error) {
+    response.status(statusCode.INTERNAL_SERVER_ERROR).json({ error });
+  }
+}
+
+async function login(request: Request, response: Response) {
+  const { user } = request as IRequest;
+
+  const token = await generateToken(user);
+
+  response.status(statusCode.OK).json({
+    user,
+    token,
+  });
+}
+
+async function refreshToken(request: Request, response: Response) {
+  const { user } = request as IRequest;
+
+  const token = await generateToken(user);
+  response.status(statusCode.OK).json({ token });
+}
+
+export default { login, register, refreshToken };
