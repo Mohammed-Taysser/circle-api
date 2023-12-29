2c59d8e5037f094b3a286630079c85e4f781c915
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-29 17:43:23 +0200

    phase1 - add auth routes to app routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import helperRoutes from './helper.route';
+import authRoutes from './auth.route';
 
 
 const router = express.Router();
 
 router.use('/', helperRoutes);
+router.use('/auth', authRoutes);
 
 
 export default router;
