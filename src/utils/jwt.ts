e673fb883a206ac7245e4cd6dc5a025aeb7bacd2
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-29 17:45:38 +0200

    phase1 - create jwt utils

---
+++ src/utils/jwt.ts
@@ -0,0 +1,25 @@
+import jwt, { VerifyOptions } from 'jsonwebtoken';
+import { JwtTokenPayload } from 'types/app';
+import CONFIG from '../core/config';
+
+async function generateToken(user: JwtTokenPayload) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+    },
+    CONFIG.jwt.secret,
+    {
+      expiresIn: CONFIG.jwt.life,
+    }
+  );
+}
+
+function verifyToken(token: string, options?: VerifyOptions) {
+  return jwt.verify(token, CONFIG.jwt.secret, options) as JwtTokenPayload;
+}
+
+export { generateToken, verifyToken };
