61430d41f70c144f543408bc01595593291f18da
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-30 16:24:29 +0200

    phase1 - add users routes to app routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
-import helperRoutes from './helper.route';
 import authRoutes from './auth.route';
-
+import helperRoutes from './helper.route';
+import usersRoutes from './user.route';
 
 const router = express.Router();
 
 router.use('/', helperRoutes);
 router.use('/auth', authRoutes);
-
+router.use('/users', usersRoutes);
 
 export default router;
