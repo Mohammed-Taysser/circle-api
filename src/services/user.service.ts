06b53e1320590f5f10f6dd39a2b6e99c0b9a933d
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-30 16:56:34 +0200

    phase1 - create user services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,53 @@
-import { UserSaveBody, UserUpdateBody } from 'types/user';
+import { FilterQuery } from 'mongoose';
+import { User, UserSaveBody, UserUpdateBody } from 'types/user';
 import schema from '../schema/user.schema';
 
+function countDocuments(filters?: FilterQuery<User>) {
+  return schema.countDocuments(filters);
+}
+
+function all() {
+  return schema.find().populate('badges');
+}
+
+function filter(filters: FilterQuery<User>) {
+  return schema.find(filters).populate('badges');
+}
+
+function getByEmail(email: string) {
+  return schema.findOne({ email }).populate('badges');
+}
+
+function getByUsername(username: string) {
+  return schema.findOne({ username }).populate('badges');
+}
+
+function getById(id: string) {
+  return schema.findById(id).populate('badges');
+}
+
+function save(body: UserSaveBody) {
+  return new schema(body).save().then((user) => user.toObject());
+}
+
+function deleteUser(id: string) {
+  return schema.findByIdAndDelete(id).populate('badges');
+}
+
+function update(id: string, body: UserUpdateBody) {
+  return schema.findByIdAndUpdate(id, body, { new: true }).populate('badges');
+}
+
 const service = {
-  all: () => schema.find(),
-  getByEmail: (email: string) => schema.findOne({ email }),
-  getByUsername: (username: string) => schema.findOne({ username }),
-  getById: (id: string) => schema.findById(id),
-  save: (body: UserSaveBody) =>
-    new schema(body).save().then((user) => user.toObject()),
-  delete: (id: string) => schema.findByIdAndDelete(id),
-  update: (id: string, body: UserUpdateBody) =>
-    schema.findByIdAndUpdate(id, body, { new: true }),
+  all,
+  countDocuments,
+  getByEmail,
+  getByUsername,
+  getById,
+  save,
+  delete: deleteUser,
+  update,
+  filter,
 };
 
 export default service;
