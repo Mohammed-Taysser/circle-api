6ca992950b7d5a717135dc8186e47f5500196cfa
mohammed-taysser <mohamedtaysser983@gmail.com>
2024-01-05 22:11:01 +0200

    phase1 - rename badges service

--- src/controllers/badge.controller.ts
+++ src/controllers/badge.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import statusCode from 'http-status-codes';
-import service from '../services/badge.service';
+import service from '../services/badge.services';
 import userSchema from '../schema/user.schema';
 import cloudinary from '../utils/cloudinary';
 import { calculatePagination } from '../utils/pagination';
